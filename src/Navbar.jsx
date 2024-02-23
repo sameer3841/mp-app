901f7f3e7ecab59e8ab90c73a64d0effc597b8b1
valerylouis <louisvalery2022@gmail.com>
2024-02-23 12:01:04 -0500

    navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Link, useMatch, useResolvedPath} from 'react-router-dom'
+
+export const Navbar = () => {
+
+    const path = window.location.pathname
+
+  return (
+    <nav className='navbar'>
+        <Link to="/" className="site-title">Site Name</Link>
+        <ul>
+            <CustomLink to="/home">Home</CustomLink>
+            <CustomLink to="/about">About</CustomLink>
+            <CustomLink to="/more">More</CustomLink>
+        </ul>
+    </nav>
+  )
+}
+
+function CustomLink({to, children, ...props}){
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({path: resolvedPath.pathname, end:true})
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to}{...props}>{children}</Link>
+        </li>
+    )
+}
